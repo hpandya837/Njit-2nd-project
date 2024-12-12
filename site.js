81d2292fd0addb10ffaa38e1b4bb2cbed842ff61
hpandya837 <callnirav1234@gmail.com>
2024-12-12 11:53:44 -0500

    Up till step 6 done

--- site.js
+++ site.js
@@ -34,6 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title:"IMDB + Hardav's Top 8 Movies",
+            owner: "Hardav",
+            github:"https://github.com/hpandya837/Njit-2nd-project",
          
       }
     },
