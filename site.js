5a484984d33083a0b623a37644ffe5c0a67bbd64
hpandya837 <callnirav1234@gmail.com>
2025-01-06 22:24:11 -0500

    donezo

--- site.js
+++ site.js
@@ -22,80 +22,84 @@
 
 
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title:"IMDB + Hardav's Top 8 Movies",
-            owner: "Hardav",
-            github:"https://github.com/hpandya837/Njit-2nd-project",
-         
-      }
-    },
-    methods: {
-      /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      getMonthText: function (dateArray) {
-        const year = dateArray[0]
-        const day = dateArray[2]
-        let monthText = " ";
-        switch (dateArray[1]) {
-          case 1:
-            monthText = "January";
-            break;
-          case 2:
-            monthText = "February";
-            break;
-          case 3:
-            monthText = "March";
-            break;
-          case 4:
-            monthText = "April";
-            break;
-          case 5:
-            monthText = "May";
-            break;
-          case 6:
-            monthText = "June";
-            break;
-          case 7:
-            monthText = "July";
-            break;
-          case 8:
-            monthText = "August";
-            break;
-          case 9:
-            monthText = "September";
-            break;
-          case 10:
-            monthText = "October";
-            break;
-          case 11:
-            monthText = "November";
-            break;
-            case 12:
-            monthText = "December";
-            break;
-          default:
-            monthText = "Invalid";
-        }
-        return `${monthText} ${day}, ${year}`;
-      },
-
-      getPosterIndex: function(currentIndex,posters){
-        return (currentIndex % posters.lenght) +1;
-      }
-      
-  
-    },
-  });
-  
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created () {
+        fetch('movies.json').then(response => response.json()).then(json => {
+              this.movies = json
+        })
+  },
+  data() {
+    return {
+        // This holds your movies.json data.
+        movies: [],
+        /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+        title: "IMDB + Hardav's Top 8 Movies",
+        owner: 'Hardav Pandya',
+        github: 'https://github.com/hpandya837'
+  }
+},
+  methods: {
+        /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+        getMonthText: function(dateArray) {
+              let month = ""
+              switch(dateArray[1]) {
+                    case 1:
+                          month="January"
+                          break
+                    case 2:
+                          month="February"
+                          break
+                    case 3:
+                          month="March"
+                          break
+                    case 4:
+                          month="April"
+                          break
+                    case 5: 
+                          month="May"
+                          break
+                    case 6:
+                          month="June"
+                          break
+                    case 7:
+                          month="July"
+                          break
+                    case 8:
+                          month="August"
+                          break
+                    case 9: 
+                          month="September"
+                          break
+                    case 10: 
+                          month="October"
+                          break
+                    case 11: 
+                          month="November"
+                          break
+                    case 12:
+                          month="December"
+                          break
+                    
+              }
+              return month + " " + dateArray[2] + ", " + dateArray[0]
+        },
+        posterClick(index){
+              if(this.movies[index].posterindex < this.movies[index].posters.length-1){
+                    this.movies[index].posterindex++
+              }
+              else {
+                    this.movies[index].posterindex = 0
+              }
+        },
+        timeText(minutes){
+              const hours = Math.trunc(minutes / 60)
+              const mins = minutes - (hours * 60)
+              return `${hours}h ${mins}m`
+              
+        },
+        
+  }
+})
 
 vue_app.mount("#vue_app")
